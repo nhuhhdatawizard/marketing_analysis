6cc3e62e2020548406bf19e451e723053f3a9124
nhuhhdatawizard <nhuhhpy@gmail.com>
2025-04-27 23:20:26 +0700

    Add files via upload

---
+++ fact_customer_reviews.sql
@@ -0,0 +1,13 @@
+
+SELECT 
+    ReviewID,  
+    CustomerID,  
+    ProductID,  
+    ReviewDate, 
+    Rating,  
+    REPLACE(ReviewText, '  ', ' ') AS ReviewText
+FROM 
+    dbo.customer_reviews;  
+
+
+	
